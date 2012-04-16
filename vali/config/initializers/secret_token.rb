febb9e46dd8391c64dac04f3134e94601bbbd0b3
Taku Yamada <t-yamada@groovenauts.jp>
2012-04-16 19:40:24 +0900

    rails new vali

---
+++ vali/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vali::Application.config.secret_token = '7bbf31174e3fc669b24cf0b731288c80cdb442d5dcf9d6ec6179124ec0a11341a910f593e161cc97986c8db57e153d7eea7287545da9438ab5883b96d761f854'
